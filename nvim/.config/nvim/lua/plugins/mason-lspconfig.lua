286acb874973f6fcbfd42e8cf2a9deaceaa6ed82
Trung Tran <trandinhtrung176@gmail.com>
2024-06-01 22:39:03 +0700

    refactor(nvim): update config

--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -4,93 +4,113 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 local utils = require("lsp.utils")
 
 require("nlspsettings").setup({
-	config_home = vim.fn.stdpath("config") .. "/nlsp-settings",
-	append_default_schemas = true,
-	ignored_servers = {},
-	loader = "json",
+  config_home = vim.fn.stdpath("config") .. "/nlsp-settings",
+  append_default_schemas = true,
+  ignored_servers = {},
+  loader = "json",
 })
 
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"bashls",
-		"clangd",
-		"jsonls",
-		"lua_ls",
-		"dockerls",
-	},
-	automatic_installation = true,
+  ensure_installed = {
+    "bashls",
+    "clangd",
+    "jsonls",
+    "lua_ls",
+    "dockerls",
+  },
+  automatic_installation = true,
 })
 
 local lspconfig = require("lspconfig")
 
 require("mason-lspconfig").setup_handlers({
-	function(server_name)
-		lspconfig[server_name].setup({
-			on_attach = utils.common_on_attach,
-			capabilities = capabilities,
-			init_options = {
-				userLanguages = {
-					eelixir = "html-eex",
-					eruby = "erb",
-					rust = "html",
-				},
-			},
-		})
-	end,
-	["lua_ls"] = function()
-		lspconfig["lua_ls"].setup({
-			on_attach = utils.common_on_attach,
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim", "use" },
-					},
-				},
-			},
-		})
-	end,
-  ["clangd"] = function ()
-		lspconfig["clangd"].setup({
+  function(server_name)
+    lspconfig[server_name].setup({
+      on_attach = utils.common_on_attach,
+      capabilities = capabilities,
+      init_options = {
+        userLanguages = {
+          eelixir = "html-eex",
+          eruby = "erb",
+          rust = "html",
+        },
+      },
+    })
+  end,
+  ["lua_ls"] = function()
+    lspconfig["lua_ls"].setup({
+      on_attach = utils.common_on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim", "use" },
+          },
+        },
+      },
+    })
+  end,
+  ["clangd"] = function()
+    lspconfig["clangd"].setup({
       filetypes = { "c", "cpp", "objc", "objcpp" },
     })
   end,
-	["tsserver"] = function()
-		require("lspconfig").tsserver.setup({
-			on_init = function(client)
-				client.server_capabilities.documentFormattingProvider = false
-			end,
-			on_attach = function(client, bufnr)
-				utils.common_on_attach(client, bufnr)
-				client.resolved_capabilities.document_formatting = false
-				client.resolved_capabilities.document_range_formatting = false
-			end,
-			commands = {
-				OrganizeImports = {
-					utils.organize_imports,
-					description = "Organize Imports",
-				},
-			},
-		})
-	end,
-	["eslint"] = function()
-		lspconfig.eslint.setup({
-			on_init = function(client)
-				client.server_capabilities.documentFormattingProvider = true
-			end,
-			on_attach = function(client, bufnr)
-				utils.common_on_attach(client, bufnr)
-				client.resolved_capabilities.document_formatting = true
-				client.resolved_capabilities.document_range_formatting = true
-			end,
-			capabilities = capabilities,
-		})
-	end,
+  ["cssls"] = function()
+    lspconfig["cssls"].setup({
+      on_attach = utils.common_on_attach,
+      capabilities = capabilities,
+
+      settings = {
+        css = {
+          lint = {
+            unknownAtRules = "ignore",
+          }
+        },
+        scss = {
+          validate = true,
+        },
+        less = {
+          validate = true,
+        },
+      },
+    })
+  end,
+  ["tsserver"] = function()
+    require("lspconfig").tsserver.setup({
+      on_init = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+      end,
+      on_attach = function(client, bufnr)
+        utils.common_on_attach(client, bufnr)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+      end,
+      commands = {
+        OrganizeImports = {
+          utils.organize_imports,
+          description = "Organize Imports",
+        },
+      },
+    })
+  end,
+  ["eslint"] = function()
+    lspconfig.eslint.setup({
+      on_init = function(client)
+        client.server_capabilities.documentFormattingProvider = true
+      end,
+      on_attach = function(client, bufnr)
+        utils.common_on_attach(client, bufnr)
+        client.resolved_capabilities.document_formatting = true
+        client.resolved_capabilities.document_range_formatting = true
+      end,
+      capabilities = capabilities,
+    })
+  end,
   ["rust_analyzer"] = function()
     require("plugins.rust-tools")
-  end
+  end,
 })
 -- vim.api.nvim_create_autocmd('BufWritePre', {
 --   pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,16 +1,32 @@
+local icons = require("icons")
 require("neo-tree").setup({
-  default_component_configs = {
-    filesystem = {
-      follow_current_file = {
-        enabled = true
-      },
-    },
-    buffers = {
-      follow_current_file = {
-        enabled = true
-      },
-    },
-  }
+	filesystem = {
+		follow_current_file = {
+			enabled = true,
+		},
+	},
+	buffers = {
+		follow_current_file = {
+			enabled = true,
+		},
+	},
+	default_component_configs = {
+		git_status = {
+			symbols = {
+				-- Change type
+				added = icons.LineAdded, -- or "✚", but this is redundant info if you use git_status_colors on the name
+				modified = icons.LineModified, -- or "", but this is redundant info if you use git_status_colors on the name
+				deleted = icons.FileDeleted, -- this can only be used in the git_status source
+				renamed = icons.FileRenamed, -- this can only be used in the git_status source
+				-- Status type
+				untracked = icons.FileUnstaged,
+				ignored = icons.FileIgnored,
+				unstaged = icons.FileUnstaged,
+				staged = icons.FileStaged,
+				conflict = icons.Conflict,
+			},
+		},
+	},
 })
 
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree filesystem toggle left<CR>")
